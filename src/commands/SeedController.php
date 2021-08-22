3d876a3d25c1ca9fc559be489356598924a13f0e
Damir Sarkulin <sarkulindamir@gmail.com>
2021-08-22 05:08:17 +0500

    added seed with duplicate requests

--- src/commands/SeedController.php
+++ src/commands/SeedController.php
@@ -13,25 +13,25 @@ class SeedController extends Controller
     {
         $faker = Factory::create();
 
-//        for ($i = 1; $i <= 5; $i++) {
-//            $manager = new Manager();
-//            $manager->name = "{$faker->firstName} {$faker->lastName}";
-//            $manager->created_at = $faker->date();
-//            $manager->updated_at = $faker->date();
-//            $manager->is_works = $faker->boolean;
-//            $manager->save();
-//        }
-//
-//        for ($j = 1; $j <= 50; $j++) {
-//            $request = new Request();
-//            $request->created_at = $faker->date();
-//            $request->updated_at = $faker->date();
-//            $request->email = $faker->email;
-//            $request->phone = $faker->phoneNumber;
-//            $request->manager_id = $faker->numberBetween(1, 5);
-//            $request->text = $faker->text;
-//            $request->save();
-//        }
+        for ($i = 1; $i <= 5; $i++) {
+            $manager = new Manager();
+            $manager->name = "{$faker->firstName} {$faker->lastName}";
+            $manager->created_at = $faker->date();
+            $manager->updated_at = $faker->date();
+            $manager->is_works = $faker->boolean;
+            $manager->save();
+        }
+
+        for ($j = 1; $j <= 50; $j++) {
+            $request = new Request();
+            $request->created_at = $faker->date();
+            $request->updated_at = $faker->date();
+            $request->email = $faker->email;
+            $request->phone = $faker->phoneNumber;
+            $request->manager_id = $faker->numberBetween(1, 5);
+            $request->text = $faker->text;
+            $request->save();
+        }
 
         for ($l = 1; $l <= 3; $l++) {
             $request = new Request();
@@ -39,7 +39,7 @@ class SeedController extends Controller
             $request->updated_at = $faker->date();
             $request->email = 'sarkulindamir@gmail.com';
             $request->phone = '+77078692233';
-            $request->manager_id = 3;
+            $request->manager_id = 1;
             $request->text = $faker->text;
             $request->save();
         }
